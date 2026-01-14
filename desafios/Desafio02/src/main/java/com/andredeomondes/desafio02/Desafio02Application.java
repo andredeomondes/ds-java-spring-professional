fab01cbb4a7fc52d3dde2b2ece15a712490288e5
andre <andredeomondes.dev@gmail.com>
2026-01-14 02:52:56 -0300

    Bloco.java

--- desafios/Desafio02/src/main/java/com/andredeomondes/desafio02/Desafio02Application.java
+++ desafios/Desafio02/src/main/java/com/andredeomondes/desafio02/Desafio02Application.java
@@ -1,4 +1,4 @@
-package com.andredeomondes.sistemaEvento;
+package com.andredeomondes.desafio02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
